824238679bf732d44dca8d8447ea0cdb04604699
azielinska <azielinska@ubuntu.myguest.virtualbox.org>
2024-01-06 15:16:28 +0100

    w funkcji rysuj dodanie pozycji mrowki

--- plansza.c
+++ plansza.c
@@ -3,20 +3,29 @@
 #include <locale.h>
 #include "plansza.h"
 
-void rysuj (int m, int n){
-	for ( int j=0; j<n; j++)
+void rysuj (int m, int n, int mrowka_m, int mrowka_n, int kierunek){
+
+	for ( int j=1; j<=n; j++)
 	{
-		for ( int i=0; i<m; i++)
+		for ( int i=1; i<=m; i++)
         	{
 			printf("┌─┐");
 	        }
 		printf("\n");
-		for ( int i=0; i<m; i++)
+		for ( int i=1; i<=m; i++)
                 {
-                        printf("│ │");
+			if (i == mrowka_m && j == mrowka_n)
+			{
+				if ( kierunek == 0 ) printf("│△│");
+				if ( kierunek == 1 ) printf("│▷│");
+				if ( kierunek == 2 ) printf("│▽│");
+				if ( kierunek == 3 ) printf("│◁│");
+			
+			}
+			else printf("│ │");
                 }
 		printf("\n");
-		for ( int i=0; i<m; i++)
+		for ( int i=1; i<=m; i++)
                 {
                         printf("└─┘");
 
