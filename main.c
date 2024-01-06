95bf7d254394676550485dab83cb7942f66852b2
azielinska <azielinska@ubuntu.myguest.virtualbox.org>
2024-01-06 15:11:01 +0100

    wprowadzenie zmiennej kierunku mrowki

--- main.c
+++ main.c
@@ -3,15 +3,22 @@
 #include "plansza.h"
 
 int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Podaj dwie liczby calkowite jako argumenty programu.\n");
+    if (argc != 5) {
         return 1;
     }
 
     int m = atoi(argv[1]);
     int n = atoi(argv[2]);
 
-    rysuj(m, n);
+    int itr = atoi(argv[3]);
+    int kierunek = atoi(argv[4]);//0 - gora, 1 - prawo, 2 - dol, 3 - lewo
+	
+    rysuj(m, n, (m+1)/2, (n+1)/2, kierunek);
+    for ( int i=0 ; i<itr ; i++ ){
+
+
+    }
+    printf("%d\n",itr);
 
     return 0;
 }
