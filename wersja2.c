f26e5a250d2e8b78cd524c261fd4b3e6abf844e6
azielinska <azielinska@ubuntu.myguest.virtualbox.org>
2024-01-18 18:21:28 +0100

    dodanie funkcjonalnosci 2

--- wersja2.c
+++ wersja2.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wchar.h>
+#include <locale.h>
+
 #include "wersja2.h"
 
 int  zczytywanie(int wymiar_m, int wymiar_n, int *x, int *y, int *kierunek, int **t) {
@@ -29,63 +32,75 @@ int  zczytywanie(int wymiar_m, int wymiar_n, int *x, int *y, int *kierunek, int 
         return 1;
     }
     
-    int m = 0, n = 1;
-    int znak;
+    int m = 0, tn = 1, n;
+    char znak;
+
+    setlocale(LC_ALL, "C.UTF-8");
+    
     
     while ((znak = fgetc(plik)) != EOF) {
-        if (znak == '\n') {
-            n++;
-            m = 1;
-        } else if (znak == ' ') {
-            m++;
-        } else if (znak == 0x2588) {  // █
-            t[m][n] = 1;
-            m++;
-        } else if (znak == 0x25B3) {  // △
-            m++;
-            *x = m;
-            *y = n + 1;
-            *kierunek = 0;
-        } else if (znak == 0x25B2) {  // ▲
-            m++;
-            *x = m;
-            *y = n;
-            *kierunek = 0;
-            t[m][n] = 1;
-        } else if (znak == 0x25B7) {  // ▷
-            m++;
-            *x = m;
-            *y = n + 1;
-            *kierunek = 1;
-        } else if (znak == 0x25B6) {  // ▶
-            m++;
-            *x = m;
-            *y = n + 1;
-            *kierunek = 1;
-            t[m][n] = 1;
-        } else if (znak == 0x25BD) {  // ▽
-            m++;
-            *x = m;
-            *y = n + 1;
-            *kierunek = 2;
-        } else if (znak == 0x25BC) {  // ▼
-            m++;
-            *x = m;
-            *y = n + 1;
-            *kierunek = 2;
-            t[m][n] = 1;
-        } else if (znak == 0x25C1) {  // ◁
-            m++;
-            *x = m;
-            *y = n + 1;
-            *kierunek = 3;
-        } else if (znak == 0x25C0) {  // ◀
-            m++;
-            *x = m;
-            *y = n + 1;
-            *kierunek = 3;
-            t[m][n] = 1;
-        }
+
+		if (znak == '\n') {
+		    tn++;
+		    m = 1;
+		} else if (znak == ' ') {
+		    m++;
+		} 
+		n=tn/3;
+		if (znak == "█") {	//
+		
+		printf("m=%d,n=%d\n",m,n);
+		    t[m][n] = 1;
+		    m++;
+		} else if (znak == 0x25B3) {  // △
+		
+		    m++;
+		    *x = m;
+		    *y = n + 1;
+		    *kierunek = 0;
+		} else if (znak == 0x25B2) {  // ▲
+		
+		    m++;
+		    *x = m;
+		    *y = n;
+		    *kierunek = 0;
+		    t[m][n] = 1;
+		} else if (znak == 0x25B7) {  // ▷
+		    m++;
+		    *x = m;
+		    *y = n + 1;
+		    *kierunek = 1;
+		} else if (znak == 0x25B6) {  // ▶
+		    m++;
+		    *x = m;
+		    *y = n + 1;
+		    *kierunek = 1;
+		    t[m][n] = 1;
+		} else if (znak == 0x25BD) {  // ▽
+		printf("m=%d,n=%d\n",m,n);
+		    m++;
+		    *x = m;
+		    *y = n + 1;
+		    *kierunek = 2;
+		} else if (znak == 0x25BC) {  // ▼
+		    m++;
+		    *x = m;
+		    *y = n + 1;
+		    *kierunek = 2;
+		    t[m][n] = 1;
+		} else if (znak == 0x25C1) {  // ◁
+		    m++;
+		    *x = m;
+		    *y = n + 1;
+		    *kierunek = 3;
+		} else if (znak == 0x25C0) {  // ◀
+		    m++;
+		    *x = m;
+		    *y = n + 1;
+		    *kierunek = 3;
+		    t[m][n] = 1;
+		}
+        
     }
 
     fclose(plik);
